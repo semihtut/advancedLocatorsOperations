f6ea1796ec3c59bdcae666d29f787f1f1689d245
SemihTut <yalaantv@gmail.com>
2020-11-19 21:04:11 +0200

    s

--- src/test/java/Advanced_Locators_Exercises/xpath.java
+++ src/test/java/Advanced_Locators_Exercises/xpath.java
@@ -33,10 +33,23 @@ public class xpath extends BasePage {
         bookPrice.put(bookName,price(1));
         System.out.println("bookPrice = " + bookPrice);
 
-
         Driver.get().quit();
     }
 
+    public static int test2(int num1,int num2){
+        return num1>num2?num1+num2:num1*num2;
 
+    }
+    public static int test3(int num1,int num2){
+        int toplam = 0;
+        for(int i = num1;i<num2;i++){
+            toplam=i%2!=0?toplam+i:toplam;
+        }
+        return toplam;
+    }
 
+    public static void main(String[] args) {
+        System.out.println(test2(9,10));
+        System.out.println(test3(0, 10));
+    }
 }
