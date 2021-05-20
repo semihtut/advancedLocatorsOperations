5931fccaa8fffe7c87abc6bed2ec0c6df8fdcdb7
SemihTut <yalaantv@gmail.com>
2021-05-20 22:41:09 +0300

    nordea

--- src/test/java/Nordea.java
+++ src/test/java/Nordea.java
@@ -2,7 +2,7 @@ public class Nordea {
 
     public static void main(String[] args) {
 
-        int []smh ={1,2,31,1,2};
+        int []smh ={1,2,31,1};
         System.out.println("nordeaTest(smh) = " + nordeaTest(smh));
         /**
          * Given an array of ints, return true if the array is length 1 or more,
@@ -17,7 +17,13 @@ public class Nordea {
 
     public static boolean nordeaTest(int [] arr){
 
-        return arr.length > 1 && (arr[0] == arr[arr.length - 1]);
+        return arr.length >= 1 && (arr[0] == arr[arr.length - 1]);
+
+    }
+
+    public static boolean nordeaTest2(int [] arr){
+
+        return arr.length >= 1 && (arr[0] == arr[arr.length - 1]);
 
     }
 }
